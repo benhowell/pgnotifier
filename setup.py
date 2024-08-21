3104ecb7801f1c6f536116b4f22d5adb30ce199d
ben <ben@psychstudio.com>
2024-08-21 22:54:04 +1000

    fix setup for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='pgnotifier',
-    version='0.0.6',
+    version='0.0.7',
     install_requires=[
 	'pyrsistent>=0.20.0',
 	'psycopg>=3.2.1',
     ],
+	long_description=long_description,
+    long_description_content_type='text/markdown'
 )
+
+
